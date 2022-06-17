8dc849ecfeb06fa77b735df07bba10cca4bb1b97
Simon <simon_wzm@163.com>
2022-06-17 19:48:22 +0800

    add random bg and twikoo comment plugin

---
+++ source/js/randombg.js
@@ -0,0 +1,10 @@
+var backimg = [
+"url(https://s2.loli.net/2022/06/17/bKUOg9pnfqWkhyE.jpg)",
+"url(https://s2.loli.net/2022/06/17/u7IwhTN5debKjWZ.jpg)",
+"url(https://s2.loli.net/2022/06/17/bKUOg9pnfqWkhyE.jpg)",
+"url(https://s2.loli.net/2022/05/22/FsMvo6mDnbPra1i.png)"
+]
+
+var bgindex = Math.floor(Math.random() * backimg.length);
+
+document.getElementById("web_bg").style.backgroundImage = backimg[bgindex];
