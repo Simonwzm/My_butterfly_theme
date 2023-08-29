9a082f704b0c80f35e8a0119ce6457af7a147114
Simon <simon_wzm@163.com>
2023-08-30 00:49:28 +0800

    bg add

--- source/js/randombg.js
+++ source/js/randombg.js
@@ -3,7 +3,8 @@ var backimg = [
 "url(https://s2.loli.net/2022/06/17/u7IwhTN5debKjWZ.jpg)",
 "url(https://s2.loli.net/2022/06/17/bKUOg9pnfqWkhyE.jpg)",
 "url(https://s2.loli.net/2022/05/22/FsMvo6mDnbPra1i.png)",
-"url(https://s2.loli.net/2022/09/07/cOT3ioRNJLkBMVf.jpg)"
+"url(https://s2.loli.net/2022/09/07/cOT3ioRNJLkBMVf.jpg)",
+"url(https://s2.loli.net/2023/08/10/1xvXpzsGIProy6M.jpg)"
 ]
 
 var bgindex = Math.floor(Math.random() * backimg.length);
